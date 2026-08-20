b9b42d2ba4473483d4acb0fa78ccf22412f51e2b
adhemeikishi <jerad.adhem@gmail.com>
2026-08-20 04:40:17 +0200

    🎉 first commit

--- backend/src/main/java/com/qrmenu/common/SecurityConfig.java
+++ backend/src/main/java/com/qrmenu/common/SecurityConfig.java
@@ -3,8 +3,8 @@ package com.qrmenu.common;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -58,7 +58,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
                 )
-                .httpBasic(HttpBasicConfigurer::init);
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
